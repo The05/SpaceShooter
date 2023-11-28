6bc0e31f0b694b6a2fbf7af72894747800ef8859
Panakssllak <Theo.olovsson@gmail.com>
2023-11-28 14:31:28 +0100

    ngfgf

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float minX = -0.870f;
+    public float maxX = 0.870f;
+    public float minY = 0.3f;
+    public float maxY = 1f;
+
+    private void Update()
+    {
+        // Get input for movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate target position
+        Vector3 targetPosition = transform.position +
+            new Vector3(moveHorizontal, moveVertical, 0f) * speed * Time.deltaTime;
+
+        // Clamp target position within boundaries
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        // Move the player
+        transform.position = targetPosition;
+    }
+}
+
